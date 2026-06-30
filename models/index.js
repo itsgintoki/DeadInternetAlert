003236922424596af14bcd791b0382f7297cd54b
itsgintoki <sinhaankit12jan@gmail.com>
2026-06-30 18:52:15 +0530

    update exports in models index to include enums for watchlist and check jobs

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 export { notificationsTable } from "./notifications.models.js";
 export { refreshTokensTable } from "./tokens.models.js";
-export { watchListTable } from "./watchlist.models.js";
+export { watchListTable,watchlistTypeEnum } from "./watchlist.models.js";
 
-export { checkJobsTable } from "./checkjobs.models.js";
+export { checkJobsTable,checkJobsStatusEnum} from "./checkjobs.models.js";
 
 export { UsersTable } from "./users.models.js";
 
